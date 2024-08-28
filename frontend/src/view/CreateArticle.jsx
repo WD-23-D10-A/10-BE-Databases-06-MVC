b57b110a4756a93c8fb5bde34a9cd7364f082885
Ari Razab <arianrazab@yahoo.com>
2024-08-28 09:38:13 +0200

    finished

--- frontend/src/view/CreateArticle.jsx
+++ frontend/src/view/CreateArticle.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CreateArticle = () => {
-  return <div>CreateArticle</div>;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("");
+  const navigate = useNavigate();
+
+  const url = import.meta.env.VITE_BACKEND_URL;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newArticle = { title, content, author };
+
+    await fetch(`${url}/api/articles`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newArticle),
+    });
+
+    navigate("/");
+  };
+
+  return (
+    <div className="container">
+      <h1>Create a new Article</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Title:</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Content:</label>
+          <input
+            type="text"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Author:</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </div>
+        <button type="submit"> Create Article</button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateArticle;
